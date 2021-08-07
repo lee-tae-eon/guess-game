9a7ec3306264c4a046e18e3b1de4426631ad4f6d
lee-tae-eon <mktears0609@gmail.com>
2021-08-08 06:53:08 +0900

    socket test

--- src/server.js
+++ src/server.js
@@ -1,17 +1,25 @@
 import express from "express";
 import { join } from "path";
-import soketIO from "soket.io";
+import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 
 app.get("/", (req, res) => res.render("home"));
 
 const handleListen = () =>
-  console.log(`✅ server Running: http://localhost:4000`);
+  console.log(`✅ server Running: http://localhost:${PORT}`);
 
-app.listen(PORT, handleListen);
+const server = app.listen(PORT, handleListen);
+
+const io = socketIO(server);
+
+io.on("connection", (socket) => {
+  socket.on("helloguys", () => console.log("Client: Hello"));
+});
--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,6 @@
-hello;
+// eslint-disable-next-line no-undef
+const socket = io("/");
+
+socket.on("hello", () => console.log("Somebody Join"));
+
+setTimeout(() => socket.emit("helloguys"), 2000);
